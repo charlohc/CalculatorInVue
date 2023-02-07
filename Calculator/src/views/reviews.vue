6d1269618fd5301e4fc964e8434a8b03caa80d6f
charlohc <Charlottee.Christiansen@gmail.com>
2023-02-07 20:23:28 +0100

    Finished functionality review page, need better css

--- Calculator/src/views/reviews.vue
+++ Calculator/src/views/reviews.vue
@@ -1,48 +1,93 @@
-
 <template>
-<h1>Here you can review the calculator</h1>
+<form class="review-form" @submit.prevent="onSubmit">
+<h3>Leave a review</h3>
 
-  <div class = "wrapperFields">
-    <p>Name</p>
-    <input id="nameField" contenteditable="true" value="Kari">
-    <p>Mail</p>
-    <input id="mailField" contenteditable="true" value="Kari.Knutsen@gmail.com"/>
-    <p>Message</p>
-    <input id="messageField" contenteditable="true" value="Veeeldig kul kalkulator wowow"/>
-
-    <button id = "submit" @click = "acceptedReview()"> submit</button>
+  <div  v-if="reviews.length" class="review-container">
+    <h3>Reviews:</h3>
+    <ul>
+      <li v-for="(review, index) in reviews" :key="index">
+        {{ review.name }} thinks this page is {{ review.rating }}
+        <br/>
+        "{{ review.text}}"
+        <br/>
+        reachable at: {{review.mail}}
+      </li>
+    </ul>
   </div>
 
-</template>
+  <label for="name">Name:</label>
+  <input id="name" v-model="name">
 
-<script>
+  <label for="mail">Mail:</label>
+  <input id="mail" v-model="mail">
+
+  <label for="rating">Rating:</label>
+  <select id="rating" v-model.number="rating">
+  <option>Very cool</option>
+  <option>Very nice</option>
+  <option>ok</option>
+  <option>meh</option>
+  <option>bad</option>
+  </select>
+
+  <label for="review">pls explain:</label>
+  <textarea id="review" v-model="text"></textarea>
+
+  <input class="button" type="submit" value="Submit">
+  </form>
+
+  </template>
+  <style scoped lang="scss"></style>
+  <script>
 
 export default {
-  name: "reviews",
-  methods: {
-    acceptedReview () {
-      if (this.acceptedName() && this.acceptedMail() && this.acceptedMessage()) {
-        console.log("Success!");
-      } else {
-        console.log("fail");
+  data() {
+    return {
+      name: '',
+      mail: '',
+      text: '',
+      rating: null,
+      reviews: [],
+      props: {
+        reviews: {
+          type: Array,
+          required: true
+        }
       }
-    },
-    acceptedName() {
-
- },
-
-  acceptedMail() {
-  return false;
-},
-
- acceptedMessage() {
-  return false;
+    }
   },
+  methods: {
+    onSubmit() {
+      if (this.name === " " || this.mail ===" " || this.text ===" " || this.rating === null) {
+        alert("Review is incomplete. Please fill out every field.")
+        return
+      }
+
+      const review = {
+        name: this.name,
+        mail: this.mail,
+        text: this.text,
+        rating: this.rating
+      }
+
+     if (this.name.match(".*\\d.*")) {
+       alert("Invalid name")
+
+     } else if (!this.mail.match(".") || !this.mail.match("@")) {
+       alert("invalid mail")
+
+     } else {
+       this.addReview(review);
+       this.name = ""
+       this.mail = ""
+       this.text = ""
+       this.rating = null
+     }
+    },
+    addReview(review) {
+      this.reviews.push(review);
+    }
   }
 }
 
-
 </script>
-<style scoped>
-
-</style>
